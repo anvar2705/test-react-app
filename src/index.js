629108bf5dbeb306e1b6c83760d7cf01c81e1d84
Appazov <anvar978@yandex.ru>
2022-12-01 23:42:05 +0500

    Add Page404

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Page404 from "./Page404";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<App />}/>
+        <Route path={'*'} element={<Page404 />}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
